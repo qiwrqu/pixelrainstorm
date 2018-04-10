b4daf8961bf21e9f467eaff294944f57f3214416
Bluesand <37777861+bluesandrain@users.noreply.github.com>
2018-04-10 01:55:58 -0300

    Modificacao da classe Rgb

--- src/imagem/Imagem.java
+++ src/imagem/Imagem.java
@@ -4,11 +4,13 @@ import java.awt.image.BufferedImage;
 
 public class Imagem {
     private BufferedImage imagemInterna;
+    private Rgb corRgb;
     private final int width;
     private final int height;
     
     public Imagem(BufferedImage imagemInterna) {
         this.imagemInterna = imagemInterna;
+        this.corRgb = new Rgb(imagemInterna);
         this.width = imagemInterna.getWidth();
         this.height = imagemInterna.getHeight();
     }
--- src/imagem/Rgb.java
+++ src/imagem/Rgb.java
@@ -1,25 +1,29 @@
 package imagem;
 
-import java.util.ArrayList;
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorModel;
 
 public class Rgb {
-    private ArrayList<Integer> red;
-    private ArrayList<Integer> green;
-    private ArrayList<Integer> blue;
+    //Arrays sao usadas ao inves de ArrayList pois ArrayList usa muita CPU e RAM.
+    private int[] red;
+    private int[] green;
+    private int[] blue;
     
     public Rgb(BufferedImage imagem) {
-        //ColorModel e usado para converter os valores de cada pixel para o formato
-        //que o programa ira utilizar
-        ColorModel colorModel = imagem.getColorModel();
         int height = imagem.getHeight();
         int width = imagem.getWidth();
-        int[] coresRgb = imagem.getRGB(0, 0, width, height, null, width, 0);
-        for (int pixel : coresRgb) {
-            red.add(colorModel.getRed(pixel));
-            green.add(colorModel.getGreen(pixel));
-            blue.add(colorModel.getBlue(pixel));
+        red = new int[width * height];
+        green = new int[width * height];
+        blue = new int[width * height];
+        int[] coresRgb = imagem.getRGB(0, 0, width, height, null, 0, width);
+        for (int i = 0; i < width * height; i++) {
+            /*Uso de bitshifts para calcular cada pixel. Os pixels que o metodo
+            *getRGB acima retorna estao no formato ARGB (Alpha, Red, Green, Blue),
+            *cada letra representando um byte do int. Os bitshifts transferem o
+            *byte da cor para o ultimo byte do int, que e recuperado com o AND 
+            *realizado com o numero 0xff (ultimos 8 bits 1, outros sao 0).*/
+            red[i] = coresRgb[i] >> 16 & 0xff;
+            green[i] = coresRgb[i] >> 8 & 0xff;
+            blue[i] = coresRgb[i] & 0xff;
         }
     }
 }
